48a67bcfd74ba767c94945137a3b7d805c8999e4
Noah Msekni <msekninoah@gmail.com>
2022-06-16 16:04:43 +0200

    Fix password matching, add photo credits and login
    Add red border to fields required to be filled
    Add red text to confirm password and password when not matching.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 var password = document.getElementById("password")
   , confirm_password = document.getElementById("confirm_password");
-
+  var errormsg = document.getElementById("errormsg")
+  var passwordLabel = document.getElementById("passwordLabel")
+  var confirmLabel = document.getElementById("confirmLabel")
 function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords do not match");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
+  if(password.length != 0) {
+    if(password.value != confirm_password.value) {
+      confirm_password.classList.toggle('invalid')
+      password.classList.toggle('invalid')
+      passwordLabel.classList.add('error')
+      confirmLabel.classList.add('error')
+      errormsg.textContent = 'Passwords do not match'
+      errormsg.style.color = 'rgba(100, 0, 0, 0.900)'
+      confirm_password.setCustomValidity("Passwords do not match");
+    }
+     else if (password.value == confirm_password.value){
+      passwordLabel.classList.remove('error')
+      confirmLabel.classList.remove('error')
+      errormsg.textContent = 'Passwords match'
+      errormsg.style.color = 'rgba(99, 162, 217, 0.553)'
+      confirm_password.setCustomValidity('');
+    }
+} 
 }
 
 password.onchange = validatePassword;
+confirm_password.onchange = validatePassword;
+
 confirm_password.onkeyup = validatePassword;
+password.onkeyup = validatePassword;
